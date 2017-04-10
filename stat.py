97778a1697d4c1812a231ac100e93fea2dc3bed8
tongtzeho <tangzihao@pku.edu.cn>
2017-04-10 17:45:20 +0800

    Update stat.py

--- stat.py
+++ stat.py
@@ -44,7 +44,7 @@ market_name = {
 	26: '应用酷'
 }
 
-stat_item = ('Download', 'Rating', 'Rating_Num', 'Edition', 'Update_Time')
+stat_item = ('Download', 'Rating', 'Rating_Num', 'Edition', 'Update_Time', 'MD5_SHA256')
 
 if __name__ == '__main__':
 	conn = pymysql.connect(host='localhost', port=3306, user='root', password='pkuoslab', db='Android', charset='utf8')
@@ -52,36 +52,39 @@ if __name__ == '__main__':
 	result = []
 	for package in package_list:
 		package_result = []
+		md5_sha256_list = []
 		for market in market_list:
-			ifexists = cursor.execute("select ID from Market_APP_Metadata where Package_Name = '"+package+"' and MarketID="+str(market))
+			ifexists = cursor.execute("select Market_APK_ID from Market_APP_Metadata where Package_Name = '"+package+"' and MarketID="+str(market))
 			if ifexists == 0:
 				package_market_result = {
 					'Download': None,
 					'Rating': None,
 					'Rating_Num': None,
 					'Edition': None,
-					'Update_Time': None
+					'Update_Time': None,
+					'MD5_SHA256': None
 				}
 				package_result.append(package_market_result)
 				print("Not Found "+package+" In "+market_name[market])
 			else:
-				cursor.execute("select ID from Market_Time_Metadata where Package_Name = '"+package+"' and MarketID="+str(market)+" and Time = (select max(Time) from Market_Time_Metadata where Package_Name = '"+package+"' and MarketID="+str(market)+")")
-				time_id = cursor.fetchall()[0][0]
-				cursor.execute("select Market_APK_ID from Market_APP_Metadata where Package_Name = '"+package+"' and MarketID="+str(market))
 				apk_id = cursor.fetchall()[0][0]
-				cursor.execute("select Downloads, Avg_rating, Total_rating from Market_Time_Metadata where ID="+str(time_id))
+				cursor.execute("select Downloads, Avg_rating, Total_rating from Market_Time_Metadata where Package_Name = '"+package+"' and MarketID="+str(market)+" and Time = (select max(Time) from Market_Time_Metadata where Package_Name = '"+package+"' and MarketID="+str(market)+")")
 				time_metadata = cursor.fetchall()[0]
-				cursor.execute("select Version, UpTime from Market_APK_Metadata where ID="+str(apk_id))
+				cursor.execute("select Version, UpTime, MD5, SHA256 from Market_APK_Metadata where ID="+str(apk_id))
 				apk_metadata = cursor.fetchall()[0]
 				package_market_result = {
 					'Download': time_metadata[0],
 					'Rating': time_metadata[1],
 					'Rating_Num': time_metadata[2],
 					'Edition': apk_metadata[0],
-					'Update_Time': None
+					'Update_Time': None,
+					'MD5_SHA256': None
 				}
 				if apk_metadata[1] != None:
 					package_market_result['Update_Time'] = datetime.datetime.fromtimestamp(float(apk_metadata[1])).strftime('%Y-%m-%d %H:%M:%S')[:10]
+				md5_sha256 = apk_metadata[2]+'-'+apk_metadata[3]
+				if not md5_sha256 in md5_sha256_list: md5_sha256_list.append(md5_sha256)
+				package_market_result['MD5_SHA256'] = md5_sha256_list.index(md5_sha256)
 				package_result.append(package_market_result)
 				print("Finish "+package+" In "+market_name[market])
 		result.append(package_result)
